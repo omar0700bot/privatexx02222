b04de1c6005d28f3b428c0b7fdb98761e0ebcbf9
omar0700bot <59139063+omar0700bot@users.noreply.github.com>
2020-08-23 00:11:45 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "x";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Coblat ..`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+console.log("Streamstatus by DayZoonHD")
+
+client.user.setActivity(`Coblat ..`, {
+type: "Playing",
+url: "https://www.twitch.tv/ee"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
+});
+
+
+client.login(process.env.BOT_TOKEN);
